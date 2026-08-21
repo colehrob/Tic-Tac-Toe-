781e99dc12ab8b20b5c037bf82a9b51c871ba21f
crob <colemanhroberts@gmail.com>
2026-08-20 21:34:21 -0700

    Refactored lobby for game join/creation

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -1,69 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import socket from "../socket";
+import "./Lobby.css";
 
 function Lobby() {
   const [gameAction, setGameAction] = useState(null);
-  const [gameType, setGameType] = useState(null);
   const navigate = useNavigate();
 
-  function handleGameType(type) {
-    setGameType(type);
+  useEffect(() => {
+    socket.on("gameCreated", () => {
+      navigate("/game");
+    });
 
-    if (gameAction === "create") {
-      navigate("/game", {
-        state: {
-          gameAction: "create",
-          gameType: type,
-        },
-      });
-    }
+    socket.on("gameStarted", () => {
+      navigate("/game");
+    });
 
-    if (gameAction === "join") {
-      
-      if (type === "public") {
-        console.log("Show public games");
-      }
+    socket.on("noGamesAvailable", () => {
+      alert("There are no games available right now.");
+    });
 
-      if (type === "private") {
-        console.log("Show room code input");
-      }
-    }
+    return () => {
+      socket.off("gameCreated");
+      socket.off("gameStarted");
+      socket.off("noGamesAvailable");
+    };
+  }, [navigate]);
+
+  function handleCreateGame() {
+    socket.emit("createGame");
+  }
+
+  function handleJoinGame() {
+    socket.emit("joinGame");
   }
 
   return (
     <div>
       <h1>Game Lobby</h1>
 
-      {/* First question */}
       {!gameAction && (
         <div>
           <h2>What would you like to do?</h2>
 
-          <button onClick={() => setGameAction("create")}>
+          <button
+            onClick={() => {
+              setGameAction("create");
+              handleCreateGame();
+            }}
+          >
             Create a Game
           </button>
 
-          <button onClick={() => setGameAction("join")}>
+          <button
+            onClick={() => {
+              setGameAction("join");
+              handleJoinGame();
+            }}
+          >
             Join a Game
-          </button>
-        </div>
-      )}
-
-      {/* Second question */}
-      {gameAction && !gameType && (
-        <div>
-          <h2>
-            {gameAction === "create"
-              ? "What type of game would you like to create?"
-              : "What type of game would you like to join?"}
-          </h2>
-
-          <button onClick={() => handleGameType("public")}>
-            Public
-          </button>
-
-          <button onClick={() => handleGameType("private")}>
-            Private
           </button>
         </div>
       )}
